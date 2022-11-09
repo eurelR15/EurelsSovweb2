bc658dfa92d442e74b5044afb38bbe1b88859e80
eurelR15 <116075531+eurelR15@users.noreply.github.com>
2022-11-09 15:23:26 +0200

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import{ motion} from "framer-motion";
 import {Skill as SkillType} from "../typings"
 import Skill from ".//Skill";
@@ -15,10 +15,10 @@ function Skills({skills}: Props){
     initial={{ opacity:0}}
     whileInView={{opacity:1}}
     transition={{duration:1.5}}
-     className=' flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
-    <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
+     className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
   
-    <div className='grid grid-cols-4 gap-5'>
+    <div className="grid grid-cols-4 gap-5">
       <h4>
         Html,Css,Java,JavaScript,Sql,Python,Typescript,Tdd,React,Tailwind,Next
 
