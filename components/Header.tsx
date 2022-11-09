5c616ab68e77cc0b7e016b12928933dee9064144
eurelR15 <116075531+eurelR15@users.noreply.github.com>
2022-11-09 15:19:17 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -9,15 +9,15 @@ type Props = {
 
 export default function Header({ }: Props) {
   return(
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center' >
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center" >
         <motion.div 
         initial={{x:-500,opacity:0,scale:0.5}}
         animate={{x:0,opacity:1,scale:1}}
         transition={{duration:1.5,}}
-        className='flex flex-row items-center'>
+        className="flex flex-row items-center">
             
 
-            <SocialIcon url="https://www.linkedin.com/in/eurel-rambaran-73663417b" fgColor='purple' bgColor='transparent' />
+            <SocialIcon url="https://www.linkedin.com/in/eurel-rambaran-73663417b" fgColor="purple" bgColor="transparent" />
         </motion.div>
         <Link href="#contact">
         <motion.div 
