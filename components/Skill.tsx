2dff4199cca31fbfbb54682cc1d0893506cdb18c
eurelr <92396243+eurelr@users.noreply.github.com>
2022-11-30 19:15:54 +0200

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 import { motion } from "framer-motion";
-import {Skill} from "../typings";
-import { urlFor } from "../sanity";
 
 type Props = {
   skill:Skill;
@@ -15,7 +13,6 @@ function Skill({skill,directionLeft}: Props) {
         initial={{x: directionLeft ? -200:200,opacity:0}}
         transition={{duration:1}}
         whileInView={{opacity:1,x:0}}
-        src={urlFor(skill?.image).url()}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"/>
     </div>
   );
