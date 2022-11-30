ff6037562622604e9126b39bc210f197ed6a2b2e
eurelr <92396243+eurelr@users.noreply.github.com>
2022-11-30 19:21:38 +0200

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,15 +2,13 @@ import React from "react"
 import { motion } from "framer-motion";
 
 type Props = {
-  skill:Skill;
-    directionLeft?:boolean;
+
 };
 
-function Skill({skill,directionLeft}: Props) {
+function Skill(}: Props) {
   return (
     <div className="group relative flex cursor-pointer">
         <motion.img
-        initial={{x: directionLeft ? -200:200,opacity:0}}
         transition={{duration:1}}
         whileInView={{opacity:1,x:0}}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"/>
